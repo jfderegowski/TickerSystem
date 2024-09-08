a11e4562aa8f5ca352c3c2a160ae37fdd5a8d372
Janek <jasiojfd@gmail.com>
2024-09-08 22:37:41 +0200

    namespaces

--- Runtime/Ticker.cs
+++ Runtime/Ticker.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using SingletonSystem.Runtime;
+using NoReleaseDate.SingletonSystem.Runtime;
 using UnityEngine;
 
 namespace NoReleaseDate.TickerSystem.Runtime
